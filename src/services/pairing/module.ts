48c86456a4deea2b2ece240e158d80feb4bc7831
Andreas Richter <708186+richtera@users.noreply.github.com>
2023-05-02 14:10:41 -0400

    fix: Lock some packages

--- src/services/pairing/module.ts
+++ src/services/pairing/module.ts
@@ -56,11 +56,11 @@ const pairingModule = (): WalletInit => {
           public chains: Chain[]
           public disconnect: EIP1193Provider['disconnect']
           // @ts-expect-error - 'emit' does not exist on `typeof EventEmitter`
-          public emit: typeof EventEmitter['emit']
+          public emit: (typeof EventEmitter)['emit']
           // @ts-expect-error - 'on' does not exist on `typeof EventEmitter`
-          public on: typeof EventEmitter['on']
+          public on: (typeof EventEmitter)['on']
           // @ts-expect-error - 'removeListener' does not exist on `typeof EventEmitter`
-          public removeListener: typeof EventEmitter['removeListener']
+          public removeListener: (typeof EventEmitter)['removeListener']
 
           private disconnected$: InstanceType<typeof Subject>
           private providers: Record<string, InstanceType<typeof StaticJsonRpcProvider>>
@@ -75,7 +75,6 @@ const pairingModule = (): WalletInit => {
             this.disconnected$ = new Subject()
             this.providers = {}
 
-            // @ts-expect-error - `payload` type (`ISessionStatus`) is not correctly `pipe`ed
             fromEvent(this.connector, ProviderEvents.WC_SESSION_UPDATE, (error, payload) => {
               if (error) {
                 throw error
@@ -94,7 +93,6 @@ const pairingModule = (): WalletInit => {
                 error: console.warn,
               })
 
-            // @ts-expect-error - `this.connector` does not satisfy the event target type
             fromEvent(this.connector, ProviderEvents.DISCONNECT, (error, payload) => {
               if (error) {
                 throw error
